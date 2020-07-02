f8d396918a510f5757fd5e8e3f8ca079a5b6e2c6
denkeel <denkeelpro@gmail.com>
2020-07-02 15:55:01 +0300

    ДОБАВИЛ СТРАНИЦЫ И К НИМ В КОНТРОЛЛЕРЕ МЕТОДЫ

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -6,8 +6,33 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    public function index() 
+    public function index()
     {
+        return __METHOD__;
+    }
 
+    public function all()
+    {
+        return __METHOD__;
+    }
+
+    public function one($id)
+    {
+        return __METHOD__ . ' Новость номер: ' . $id;
+    }
+
+    public function categories()
+    {
+        return __METHOD__;
+    }
+    
+    public function showCategory($slug)
+    {
+        return __METHOD__ . ' Категория: ' . $slug;
+    }
+
+    public function add()
+    {
+        return __METHOD__;
     }
 }
